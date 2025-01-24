4644d10ecdb9aadecbaf5595da015960db700ca7
RichieMuga <richiemugambi4@gmail.com>
2025-01-25 00:01:53 +0300

    chore: add better comments for easier description

--- Sliding_window/sliding_window.py
+++ Sliding_window/sliding_window.py
@@ -1,29 +1,36 @@
-class SlidingWIndow:
-    def __init__(self, nums, targetSum):
+# Implement sliding window technique
+class SlidingWindow:
+    def __init__(self, target, nums):
+        self.target = target
         self.nums = nums
-        self.targetSum = targetSum
 
     def sliding_window(self):
-        l = 0
-        # allocate the biggest number posiible
-        leastSubArray = float('inf')
+        # counter rep the largest possible value
+        # l rep the left counter
+        counter, l = float('inf'), 0
+        
+        # rep the total subarray
         total = 0
 
+        # starting from the right counter
         for r in range(len(self.nums)):
-            # increase the right pointer
+            # add the total values when we increase the right value and store it
             total += self.nums[r]
-
-            # while the totat is greater than the target sum, if its long enough, we move the left pointer
-            while total >= self.targetSum:
-                # get the minimum subArray between the current one and the one being checked
-                leastSubArray = min(leastSubArray, r - l + 1)
-                # remove the the left part
+            # while we have not exceeded the target with our total
+            while self.target < total:
+                # we check the minimum continous subarray
+                counter = min(counter, r-l+1)
+                # we then remove the left most value
                 total -= self.nums[l]
-                # add the left pointer
-                l += 1
+                # then increase the pointer
+                l+=1
+        return counter
+         
 
-        return leastSubArray
 
-        # check the loop and try to add the first index to the following indexuuuuuu        # if the targetSum is not exceeded
-        # increase the pointer r and then recalculate the sum from the pointer l all the way to where the current r is at 
 
+# Goal: to implement a dynamic sliding window technique
+# What the question? or specifics?
+# You are given an array nums with a target
+# find the highest number of sub array that is not more than the target
+# return the longest continiuos sub array
